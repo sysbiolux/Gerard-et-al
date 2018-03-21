472f21936961bc3261666fbe68c9d01359c983f3
Déborah Gérard <debora_gerard@hotmail.com>
2018-03-21 12:48:26 +0100

    Add the conversion of HOMER output into BED files

--- HOMER-H3K27Ac-PeakCalling.sh
+++ HOMER-H3K27Ac-PeakCalling.sh
@@ -20,3 +20,18 @@ findPeaks /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-H3K27-O-D3-TagDirector
 findPeaks /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-H3K27-O-D5-TagDirectory/ -i /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-I-O-D5-TagDirectory/ -style super -o /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-SuperEnhancers-O-D5-minDist10kb-L0 -typical /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-TypicalEnhancers-O-D5-minDist10kb-L0 -minDist 10000 -L 0
 findPeaks /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-H3K27-O-D9-TagDirectory/ -i /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-I-O-D5-TagDirectory/ -style super -o /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-SuperEnhancers-O-D9-minDist10kb-L0 -typical /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-TypicalEnhancers-O-D9-minDist10kb-L0 -minDist 10000 -L 0
 findPeaks /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-H3K27-O-D15-TagDirectory/ -i /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-I-O-D5-TagDirectory/ -style super -o /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-SuperEnhancers-O-D15-minDist10kb-L0 -typical /scratch/users/dgerard/TC1-ChIP-seq/H3K27Ac/TC1-TypicalEnhancers-O-D15-minDist10kb-L0 -minDist 10000 -L 0
+
+# Then, convert the HOMER SE files to BED format using pos2bed.pl from the HOMER suite
+## ADIPOGENESIS ##
+pos2bed.pl TC1-SuperEnhancers-ST2-D0-minDist10kb-L0 > TC1-SuperEnhancers-ST2-D0-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-A-D1-minDist10kb-L0 > TC1-SuperEnhancers-A-D1-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-A-D3-minDist10kb-L0 > TC1-SuperEnhancers-A-D3-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-A-D5-minDist10kb-L0 > TC1-SuperEnhancers-A-D5-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-A-D15-minDist10kb-L0 > TC1-SuperEnhancers-A-D15-minDist10kb-L0.bed
+
+## OSTEOBLASTOGENESIS ##
+pos2bed.pl TC1-SuperEnhancers-O-D1-minDist10kb-L0 > TC1-SuperEnhancers-O-D1-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-O-D3-minDist10kb-L0 > TC1-SuperEnhancers-O-D3-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-O-D5-minDist10kb-L0 > TC1-SuperEnhancers-O-D5-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-O-D9-minDist10kb-L0 > TC1-SuperEnhancers-O-D9-minDist10kb-L0.bed
+pos2bed.pl TC1-SuperEnhancers-O-D15-minDist10kb-L0 > TC1-SuperEnhancers-O-D15-minDist10kb-L0.bed
